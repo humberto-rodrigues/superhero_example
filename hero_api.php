9d793514e23ce16167cda945292a506e86610879
humberto-rodrigues <94197937+humberto-rodrigues@users.noreply.github.com>
2021-11-13 12:13:38 +0000

    Update hero_api.php

--- hero_api.php
+++ hero_api.php
@@ -154,44 +154,4 @@ class hero_api {
     
 }
 
-?><!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Untitled Page</title>
-<meta name="generator" content="WYSIWYG Web Builder 17 - https://www.wysiwygwebbuilder.com">
-<style>body {
-   background-color: #FFFFFF;
-   color: #000000;
-   font-family: Arial;
-   font-size: 13px;
-   font-weight: normal;
-   line-height: 1.1875;
-   margin: 0;
-   padding: 0;
-}
-a {
-   color: #0000FF;
-   text-decoration: underline;
-}
-a:visited {
-   color: #800080;
-}
-a:active {
-   color: #FF0000;
-}
-a:hover {
-   color: #0000FF;
-   text-decoration: underline;
-}
-input:focus,
-textarea:focus,
-select:focus {
-   outline: none;
-}
-</style>
-</head>
-<body>
-
-</body>
-</html>
+?>
